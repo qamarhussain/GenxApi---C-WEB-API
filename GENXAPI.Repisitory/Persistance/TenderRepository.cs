4e400e78a664d7bf369c747e20f6b265f88623ae
junaid <junaid.zahid30@outloo.com>
2019-09-14 15:29:18 +0500

    commit all

--- GENXAPI.Repisitory/Persistance/TenderRepository.cs
+++ GENXAPI.Repisitory/Persistance/TenderRepository.cs
@@ -24,7 +24,7 @@ namespace GENXAPI.Repisitory
 
         public IList<DropdownListDto> GetContractKeyPair(int CompanyId, int BusinessUnitId, int CustomerId)
         {
-            var result = Find(m => m.StatusId == (byte)Status.Active && m.CustomerId == CustomerId && m.CompanyId == CompanyId && m.BusinessUnitId == BusinessUnitId).Select(r =>
+            var result = Find(m => m.ProceedStatus != (byte)TenderUtility.ContractCancelState && m.CustomerId == CustomerId && m.CompanyId == CompanyId && m.BusinessUnitId == BusinessUnitId).Select(r =>
           new DropdownListDto
           {
               Value = r.Id.ToString(),
